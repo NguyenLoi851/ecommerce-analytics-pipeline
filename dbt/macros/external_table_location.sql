2c7e99602d17fe18133fb6a76a5bcaf89668ad63
LoiNguyen <nguyensyloi851@gmail.com>
2026-04-03 22:34:18 +0700

    feat: update SQL scripts to use external_location for Delta Lake integration

--- dbt/macros/external_table_location.sql
+++ dbt/macros/external_table_location.sql
@@ -1,6 +1,5 @@
 {% macro external_table_location() -%}
     {{ config(
-        location_root='s3://' ~ var('raw_bucket_name') ~ '/delta/olist',
-        include_full_name_in_path=true
+        external_location='olist_delta_ext_loc'
     ) }}
 {%- endmacro %}
