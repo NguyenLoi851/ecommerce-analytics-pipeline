1cc86f65af23f167de7d892dea0299cc6a541c4a
LoiNguyen <nguyensyloi851@gmail.com>
2026-04-03 23:20:59 +0700

    Revert "Reapply "feat: update SQL scripts to use external_location for Delta Lake integration""
    This reverts commit dc654ed46a45ca25cefb54a86819210484925538.

--- dbt/models/silver/dim_products_scd2_merge.sql
+++ dbt/models/silver/dim_products_scd2_merge.sql
@@ -5,7 +5,8 @@
         unique_key='scd_id',
         on_schema_change='sync_all_columns',
         post_hook="{{ scd2_close_current_products_merge(this) }}",
-        external_location='olist_delta_ext_loc'
+        location_root='s3://' ~ var('raw_bucket_name') ~ '/delta/olist',
+        include_full_name_in_path=true
     )
 }}
 
